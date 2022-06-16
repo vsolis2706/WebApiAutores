ca1f40d92ba3dc9ce42394a7a13d56a311161d1b
vsolis2706 <57462619+vsolis2706@users.noreply.github.com>
2022-06-16 14:47:28 -0500

    Agregar archivos de proyecto.

---
+++ WebApiAutores/Controllers/AutoresController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiAutores.Entidades;
+
+namespace WebApiAutores.Controllers
+{
+    [ApiController]
+    [Route("api/Autores")]
+
+    public class AutoresController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<List<Autor>> Get()
+        {
+            return new List<Autor>()
+            {
+                new Autor()               {
+                    Id=1,
+                    Nombre="Camila"
+                },
+
+                 new Autor()               {
+                    Id=2,
+                    Nombre="Koo"
+                }
+
+            };
+        }
+
+    }
+}
---
+++ WebApiAutores/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace WebApiAutores
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("defaultConnection")));
+            {
+
+
+            } 
+
+          
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title= "WebApiAutores", Version="v1"});
+            });
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { 
+             endpoints.MapControllers();
+            });
+        }
+
+    }
+}
